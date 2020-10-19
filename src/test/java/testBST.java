492861b5e1792996eedffe7eb5d50d9756e6a690
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-19 13:24:11 +0530

    Refactored creating BST

--- src/test/java/testBST.java
+++ src/test/java/testBST.java
@@ -4,11 +4,11 @@ import org.junit.Test;
 public class testBST
 {
     @Test
-    public void givenNodes_whenInitialize_shouldReturnBinarySearchTree() {
+    public void givenNodes_shouldReturnBST() {
         BinarySearchTree bst = new BinarySearchTree();
-        bst.add(56);
-        bst.add(30);
-        bst.add(70);
+        bst.add(51);
+        bst.add(32);
+        bst.add(75);
         int size = bst.getSize();
         Assert.assertEquals(3,size);
     }
